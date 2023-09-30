7baf6f1d23d433bcd966489b41b7bf03e95eeb19
Александр Новиков <sasanovikov712@gmail.com>
2023-09-30 11:18:22 +0300

    Ваше сообщение

--- hello.pas
+++ hello.pas
@@ -1,1 +1,9 @@
-﻿print('Hello, world!');
+﻿begin
+  var h:=ReadInteger('Введи который сейчас час:');
+  case h of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!'); 
+    else Print('Доброй ночи, мир!');
+  end;
+end;
