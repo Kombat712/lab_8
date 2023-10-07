606a29060c7562943386eb8ad2ac35590697ea7b
Александр <sasanovikov712@gmail.com>
2023-10-08 00:44:01 +0300

    Дз4

--- Hello.pas
+++ Hello.pas
@@ -1,3 +1,8 @@
 ﻿begin
-  
+  var language:=ReadInteger('Если ваш язык Французкий Введи 1, Если ваш язык Английский введи 2 , если ваш язык вьетнамский введи 3');
+  case language of
+ 1: writeln('salut!');
+ 2: writeln('hello!');
+ 3: writeln('Xin chào!');
+end;
 end.
